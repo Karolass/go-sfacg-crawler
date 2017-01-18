8abaddb638f6c80f2b8552064c73100117d32ce8
Karolass <tark810145@gmail.com>
2017-01-18 10:45:52 +0800

    go fmt

--- main.go
+++ main.go
@@ -1,76 +1,76 @@
 package main
 
 import (
-    "fmt"
-    // "encoding/json"
-    // "log"
-    "time"
+	"fmt"
+	// "encoding/json"
+	// "log"
+	"time"
 )
 
 var catalogCount, chapterCount, pageCount = 0, 0, 0
 var nowPage, limit = 1, 5
 
 func main() {
-    start := time.Now()
-    run("http://comic.sfacg.com/Catalog/")
-    fmt.Printf("Total Catalog counts: %d \n", catalogCount)
-    fmt.Printf("Total Chapter counts: %d \n", chapterCount)
-    fmt.Printf("Total Page counts: %d \n", pageCount)
-    fmt.Println(time.Since(start))
+	start := time.Now()
+	run("http://comic.sfacg.com/Catalog/")
+	fmt.Printf("Total Catalog counts: %d \n", catalogCount)
+	fmt.Printf("Total Chapter counts: %d \n", chapterCount)
+	fmt.Printf("Total Page counts: %d \n", pageCount)
+	fmt.Println(time.Since(start))
 }
 
 func run(URL string) {
-    catalogs, nextPage := GetCatalogs(URL)
+	catalogs, nextPage := GetCatalogs(URL)
 
-    // bytes, err := json.MarshalIndent(catalogs, "", "    ")
-    // if err != nil {
-    //     log.Fatalln(err)
-    // }
+	// bytes, err := json.MarshalIndent(catalogs, "", "    ")
+	// if err != nil {
+	//     log.Fatalln(err)
+	// }
 
-    // fmt.Println(string(bytes))
-    // fmt.Println(nextPage)
+	// fmt.Println(string(bytes))
+	// fmt.Println(nextPage)
 
-    catalogCount += len(catalogs)
-    // get Chapters
-    runChatper(catalogs)
+	catalogCount += len(catalogs)
+	// get Chapters
+	runChatper(catalogs)
 
-    fmt.Printf("Scrape catalog page %d complete!\n", nowPage)
-    if len(nextPage) > 0 && nowPage < limit {
-        nowPage++
-        run(nextPage)
-    }
+	fmt.Printf("Scrape catalog page %d complete!\n", nowPage)
+	if len(nextPage) > 0 && nowPage < limit {
+		nowPage++
+		run(nextPage)
+	}
 }
 
 func runChatper(catalogs Catalogs) {
-    for _, catalog := range catalogs {
-        chapters := GetChapters(catalog.ID, catalog.URL)
+	for _, catalog := range catalogs {
+		chapters := GetChapters(catalog.ID, catalog.URL)
 
-        // bytes, err := json.MarshalIndent(chapters, "", "    ")
-        // if err != nil {
-        //     log.Fatalln(err)
-        // }
+		// bytes, err := json.MarshalIndent(chapters, "", "    ")
+		// if err != nil {
+		//     log.Fatalln(err)
+		// }
 
-        // fmt.Println(catalog.Title)
-        // fmt.Println(string(bytes))
+		// fmt.Println(catalog.Title)
+		// fmt.Println(string(bytes))
 
-        chapterCount += len(chapters)
-        // get Pages
-        runPage(chapters)
-    }
+		chapterCount += len(chapters)
+		// get Pages
+		runPage(chapters)
+	}
 }
 
 func runPage(chapters Chapters) {
-    for _, chapter := range chapters {
-        pages := GetPages(chapter.CatalogID, chapter.Title, chapter.URL)
+	for _, chapter := range chapters {
+		pages := GetPages(chapter.CatalogID, chapter.Title, chapter.URL)
 
-        // bytes, err := json.MarshalIndent(pages, "", "    ")
-        // if err != nil {
-        //     log.Fatalln(err)
-        // }
+		// bytes, err := json.MarshalIndent(pages, "", "    ")
+		// if err != nil {
+		//     log.Fatalln(err)
+		// }
 
-        // fmt.Println(chapter.Title)
-        // fmt.Println(string(bytes))
+		// fmt.Println(chapter.Title)
+		// fmt.Println(string(bytes))
 
-        pageCount += len(pages)
-    }
+		pageCount += len(pages)
+	}
 }
--- request.go
+++ request.go
@@ -1,149 +1,149 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "strings"
-    "regexp"
-    "io/ioutil"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
 
-    "github.com/PuerkitoBio/goquery"
+	"github.com/PuerkitoBio/goquery"
 )
 
 func GetHtml(URL string) (res *http.Response) {
-    client := &http.Client{}
+	client := &http.Client{}
 
-    req, err := http.NewRequest("GET", URL, nil)
-    if err != nil {
-        log.Fatalln(err)
-    }
+	req, err := http.NewRequest("GET", URL, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    req.Header.Set("User-Agent",
-        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.94 Safari/537.36")
+	req.Header.Set("User-Agent",
+		"Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.94 Safari/537.36")
 
-    res, err = client.Do(req)
-    if err != nil {
-        log.Fatalln(err)
-    }
+	res, err = client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    return
+	return
 }
 
-func GetCatalogs(URL string) (catalogs Catalogs, nextPage string){
+func GetCatalogs(URL string) (catalogs Catalogs, nextPage string) {
 
-    res := GetHtml(URL)
-    doc, err := goquery.NewDocumentFromResponse(res)
-    if err != nil {
-        log.Fatalln(err)
-    }
+	res := GetHtml(URL)
+	doc, err := goquery.NewDocumentFromResponse(res)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    attr, exist := doc.Find("li.pagebarNext a").Attr("href")
-    if exist {
-        nextPage = attr
-    }
+	attr, exist := doc.Find("li.pagebarNext a").Attr("href")
+	if exist {
+		nextPage = attr
+	}
 
-    doc.
-        Find(".Comic_Pic_List li:nth-child(2)").
-        Each(func(i int, this *goquery.Selection) {
+	doc.
+		Find(".Comic_Pic_List li:nth-child(2)").
+		Each(func(i int, this *goquery.Selection) {
 
-        title := this.Find("strong").Find("a").Text()
-        attr, exist := this.Find("strong").Find("a").Attr("href")
-        var ID string
-        if exist {
-            r := regexp.MustCompile(`^[\w\W]+\/HTML\/(\w+)\/$`)
-            results := r.FindStringSubmatch(attr)
-            if len(results) > 1 {
-                ID = results[1]
-            }
-        }
+			title := this.Find("strong").Find("a").Text()
+			attr, exist := this.Find("strong").Find("a").Attr("href")
+			var ID string
+			if exist {
+				r := regexp.MustCompile(`^[\w\W]+\/HTML\/(\w+)\/$`)
+				results := r.FindStringSubmatch(attr)
+				if len(results) > 1 {
+					ID = results[1]
+				}
+			}
 
-        var author, category string
-        this.
-            Find("a.Blue_link1").
-            Each(func(i int, this *goquery.Selection) {
-                if i % 2 == 0 {
-                    author = this.Text()
-                } else {
-                    category = this.Text()
-                }
-            })
+			var author, category string
+			this.
+				Find("a.Blue_link1").
+				Each(func(i int, this *goquery.Selection) {
+					if i%2 == 0 {
+						author = this.Text()
+					} else {
+						category = this.Text()
+					}
+				})
 
-        description := strings.TrimSpace(this.Find("br").Get(2).NextSibling.Data)
-        thumbnailURL, _ := this.Parent().Find("li.Conjunction").Find("img").Attr("src")
+			description := strings.TrimSpace(this.Find("br").Get(2).NextSibling.Data)
+			thumbnailURL, _ := this.Parent().Find("li.Conjunction").Find("img").Attr("src")
 
-        if len(ID) > 0 {
-            catalogs = append(catalogs, Catalog{
-                        ID: ID,
-                        Title: title,
-                        Author: author,
-                        Category: category,
-                        Description: description,
-                        URL: attr,
-                        ThumbnailURL: thumbnailURL,
-                    })
-        }
-    })
+			if len(ID) > 0 {
+				catalogs = append(catalogs, Catalog{
+					ID:           ID,
+					Title:        title,
+					Author:       author,
+					Category:     category,
+					Description:  description,
+					URL:          attr,
+					ThumbnailURL: thumbnailURL,
+				})
+			}
+		})
 
-    return
+	return
 }
 
-func GetChapters(catalogID string, URL string) (chapters Chapters){
+func GetChapters(catalogID string, URL string) (chapters Chapters) {
 
-    res := GetHtml(URL)
-    doc, err := goquery.NewDocumentFromResponse(res)
-    if err != nil {
-        log.Fatalln(err)
-    }
+	res := GetHtml(URL)
+	doc, err := goquery.NewDocumentFromResponse(res)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    doc.
-        Find("ul.serialise_list li a").
-        Each(func(i int, this *goquery.Selection) {
+	doc.
+		Find("ul.serialise_list li a").
+		Each(func(i int, this *goquery.Selection) {
 
-        var title, URL string
-        if len(this.Text()) > 0 {
-            title = this.Text()
-        } else {
-            title = this.Find("font").Text()
-        }
+			var title, URL string
+			if len(this.Text()) > 0 {
+				title = this.Text()
+			} else {
+				title = this.Find("font").Text()
+			}
 
-        attr, exist := this.Attr("href")
-        if exist {
-            URL = "http://comic.sfacg.com" + attr
-        }
+			attr, exist := this.Attr("href")
+			if exist {
+				URL = "http://comic.sfacg.com" + attr
+			}
 
-        chapters = append(chapters, Chapter{
-                    CatalogID: catalogID,
-                    Title: title,
-                    URL: URL,
-                })
-    })
+			chapters = append(chapters, Chapter{
+				CatalogID: catalogID,
+				Title:     title,
+				URL:       URL,
+			})
+		})
 
-    return
+	return
 }
 
-func GetPages(catalogID string, chapterTitle string, URL string) (pages Pages){
+func GetPages(catalogID string, chapterTitle string, URL string) (pages Pages) {
 
-    res := GetHtml(URL)
-    doc, err := goquery.NewDocumentFromResponse(res)
-    if err != nil {
-        log.Fatalln(err)
-    }
+	res := GetHtml(URL)
+	doc, err := goquery.NewDocumentFromResponse(res)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    attr, exist := doc.Find("script").Eq(1).Attr("src")
-    if exist {
-        jsURL := "http://comic.sfacg.com" + attr
+	attr, exist := doc.Find("script").Eq(1).Attr("src")
+	if exist {
+		jsURL := "http://comic.sfacg.com" + attr
 
-        res = GetHtml(jsURL)
-        bytes, _ := ioutil.ReadAll(res.Body)
-        re := regexp.MustCompile(`\/Pic\/[\w|\/]+\.\w+`)
-        for _, match := range re.FindAllString(string(bytes), -1) {
-            pages = append(pages, Page{
-                        CatalogID: catalogID,
-                        ChapterTitle: chapterTitle,
-                        URL: "http://comic.sfacg.com" + match,
-                    })
-        }
-    }
+		res = GetHtml(jsURL)
+		bytes, _ := ioutil.ReadAll(res.Body)
+		re := regexp.MustCompile(`\/Pic\/[\w|\/]+\.\w+`)
+		for _, match := range re.FindAllString(string(bytes), -1) {
+			pages = append(pages, Page{
+				CatalogID:    catalogID,
+				ChapterTitle: chapterTitle,
+				URL:          "http://comic.sfacg.com" + match,
+			})
+		}
+	}
 
-    return
+	return
 }
